5ca375bf48ef1e06fdae046959c3f3d95340e4a3
bharathjinka09 <bharathjinka09@gmail.com>
2020-06-30 16:59:24 +0530

    Added pokemon type

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 
-const apiData = {
-    url: 'https://pokeapi.co/api/v2/',
-    type: 'pokemon',
-    name: 'pikachu',
-}
+function pokemon() {
 
-const {url, type, name} = apiData
+    let pokemon = document.querySelector('input').value;
+    console.log('pokemon'+pokemon)
 
-const apiUrl = `${url}${type}/${name}`
+    const apiData = {
+        url: 'https://pokeapi.co/api/v2/',
+        type: 'pokemon',
+        name: pokemon,
+    }
+
+    const {url, type, name} = apiData
+
+    const apiUrl = `${url}${type}/${name}`
 
 
-const generateHtml = (data) => {
-    console.log(data)
-    const html = `
-    <div class="card border-success mb-3" style="width: 18rem;">
+    const generateHtml = (data) => {
+        console.log(data)
+        const html = `
+        <div class="card border-success mb-3" style="width: 18rem;">
 
-        <img width='100%;' src=${data.sprites.front_default} class="card-img-top">
-        <div class="card-body">
-            <h5 class="card-title">${data.name}</h5>
-            <div class="card-text">
-                <span>Height: ${data.height}</span>
-                <span>Weight: ${data.weight}</span>
+            <img width='100%;' src=${data.sprites.front_default} class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${data.name}</h5>
+                <div class="card-text">
+                    <span><b>Height:</b> ${data.height}</span>,
+                    <span><b>Weight:</b> ${data.weight}</span><br>
+                    <span><b>Type:</b> ${data.types[0]['type']['name']}</span> <br>
+                    <span><b>Species:</b> ${data.species.name}</span> <br>
+                </div>
             </div>
         </div>
-    </div>
-    `
-    const pokemonDiv = document.querySelector('.pokemon');
-    console.log(pokemonDiv);
-    pokemonDiv.innerHTML = html;
+        `
+        const pokemonDiv = document.querySelector('.pokemon');
+        console.log(pokemonDiv);
+        pokemonDiv.innerHTML = html;
+    }
+
+    fetch(apiUrl)
+        .then( (data) => {
+            if(data.ok){
+                return data.json()
+            }
+            throw new Error('Response not ok.'); 
+        })
+        .then( pokemon => generateHtml(pokemon))
+        .catch( error => console.error('Error:', error))
 }
-
-
-
-fetch(apiUrl)
-    .then( (data) => {
-        if(data.ok){
-            return data.json()
-        }
-        throw new Error('Response not ok.'); 
-    })
-    .then( pokemon => generateHtml(pokemon))
-    .catch( error => console.error('Error:', error))
